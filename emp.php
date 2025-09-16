a8da5b0d5d9fd1f3ee40f5e35c0f23b7eb3d8dde
relangiharika205-dot <harika292005@gmail.com>
2025-09-16 10:09:01 +0530

    Add files via upload

---
+++ emp.php
@@ -0,0 +1,13 @@
+<?php
+echo"employee details are:"."<br><br>";
+$empno=$_GET['empno];
+$empname=$_GET['empname'];
+$empphno=$_GET['empphno'];
+$address=$_GET['address'];
+echo"empno=$empno<br>";
+echo"empname=$empname<br>";
+echo"empphno=$empphno<br>";
+echo"address=$address";
+?>
+
+
